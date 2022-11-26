59d8eee77426b9c3eb604ba61924b535b296d39c
이영우 <ywlee@hanatour.com>
2022-11-26 22:35:41 +0900

    vue router 를 전역으로 추가
    - vue 인스턴스에 넣음으로써 한 프로젝트에서 router, route 에 접근이 가능하도록 함
    - router 는 이동할 수 있는 라이브러리, route 는 라우트 맵핑 정보를 볼 수 있음

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from '@/router';
 
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app');
 
 import 'bootstrap/dist/js/bootstrap.js';
