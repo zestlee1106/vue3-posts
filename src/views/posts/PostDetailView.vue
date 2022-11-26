505eca9099f5f0d670ac7a2118e6ecadd2bb1abd
이영우 <ywlee@hanatour.com>
2022-11-27 00:27:47 +0900

    CRUD 페이지 마크업 추가

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -1,13 +1,47 @@
 <template>
   <div>
-    <h2>게시글 상세</h2>
+    <h2>제목</h2>
+    <p>내용</p>
+    <p class="text-muted">2020-01-01</p>
     <hr class="my-4" />
-    {{ $route.params }}
-    {{ $route.query.searchText }}
-    {{ $route.hash }}
+    <div class="row g-2">
+      <div class="col-auto">
+        <button class="btn btn-outline-dark">이전글</button>
+      </div>
+      <div class="col-auto">
+        <button class="btn btn-outline-dark">다음글</button>
+      </div>
+      <div class="col-aoto me-auto"></div>
+      <div class="col-auto">
+        <button class="btn btn-outline-dark" @click="goListPage">목록</button>
+      </div>
+      <div class="col-auto">
+        <button class="btn btn-outline-primary" @click="goEditPage">
+          수정
+        </button>
+      </div>
+      <div class="col-auto">
+        <button class="btn btn-outline-danger">삭제</button>
+      </div>
+    </div>
+    <hr class="my-4" />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+const id = route.params.id;
+
+const goListPage = () => {
+  router.push({ name: 'PostList' });
+};
+
+const goEditPage = () => {
+  router.push({ name: 'PostEdit', params: { id } });
+};
+</script>
 
 <style lang="scss" scoped></style>
