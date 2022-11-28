c0ed6fa9a87571c1f0584413b90e8a8c5047434f
Youngwoo <zestlee1106@gmail.com>
2022-11-28 21:21:58 +0900

    json server 추가됨에 따라 api 스크립트 수정
    - axios 통신하여 데이터 가져오도록 수정함

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,41 +1,21 @@
-// axios
-const posts = [
-  {
-    id: 1,
-    title: '제목 1',
-    content: '내용 1',
-    createdAt: '2020-01-01',
-  },
-  {
-    id: 2,
-    title: '제목 2',
-    content: '내용 2',
-    createdAt: '2020-02-02',
-  },
-  {
-    id: 3,
-    title: '제목 3',
-    content: '내용 3',
-    createdAt: '2020-03-03',
-  },
-  {
-    id: 4,
-    title: '제목 4',
-    content: '내용 4',
-    createdAt: '2020-04-05',
-  },
-  {
-    id: 5,
-    title: '제목 5',
-    content: '내용 5',
-    createdAt: '2020-04-05',
-  },
-];
+import axios from 'axios';
 
 export function getPosts() {
-  return posts;
+  return axios.get('http://localhost:5555/posts');
 }
 
 export function getPostById(id) {
-  return posts.find(item => item.id === id);
+  return axios.get(`http://localhost:5555/posts/${id}`);
+}
+
+export function createPost(data) {
+  return axios.post('http://localhost:5555/posts', data);
+}
+
+export function updatePost(id, data) {
+  return axios.put(`http://localhost:5555/posts/${id}`, data);
+}
+
+export function deletePost(id) {
+  return axios.delete(`http://localhost:5555/posts/${id}`);
 }
