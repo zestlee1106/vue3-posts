43e62d36bd94c45fe60d1b0624cdc8936d9183c6
Youngwoo <zestlee1106@gmail.com>
2022-11-28 22:50:35 +0900

    필터, limit 를 위해 get parameter 를 추가하였다
    - computed 를 사용하고 싶으면 vue 에서 가져오면 된다.
    - wathEffect 안에 있는 함수의 반응형 변수가 수정될 경우 안의 함수가 콜백으로 호출이 된다.

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export function getPosts() {
-  return axios.get('http://localhost:5555/posts');
+export function getPosts(params) {
+  return axios.get('http://localhost:5555/posts', { params });
 }
 
 export function getPostById(id) {
