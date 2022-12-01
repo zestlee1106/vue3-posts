e6f9f66f13a6053a38ed02c1d717d2d9387c4f15
Younhwoo <zestlee1106@gmail.com>
2022-12-01 11:50:14 +0900

    입력 폼 공통 컴포넌트화 함
    - 컴포넌트 가져와서 쓰고 싶고 slot 을 부여하고 싶을 떈, #name 을 사용하면 된다

---
+++ src/views/posts/PostForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <form>
+    <div class="mb-3">
+      <label for="title" class="form-label">제목</label>
+      <input
+        type="text"
+        class="form-control"
+        id="title"
+        :value="title"
+        @input="$emit('update:title', $event.target.value)"
+      />
+    </div>
+    <div class="mb-3">
+      <label for="contents" class="form-label">내용</label>
+      <textarea
+        class="form-control"
+        id="contents"
+        rows="3"
+        :value="content"
+        @input="$emit('update:content', $event.target.value)"
+      ></textarea>
+    </div>
+    <div class="d-flex gap-2 mt-4">
+      <slot name="actions"> </slot>
+    </div>
+  </form>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  content: String,
+});
+defineEmits(['update:title', 'update:content']);
+</script>
+
+<style lang="scss" scoped></style>
